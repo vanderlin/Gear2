6849f5530ca0c2363a4f0a1d9f0d8c46a592e1f0
Vanderlin <tvanderlin@bo-m-tvanderlin.ideo.com>
2014-09-09 10:44:38 -0400

    timestamp the migration files

--- app/routes.php
+++ app/routes.php
@@ -70,60 +70,6 @@ Route::group(array('before'=>'siteprotection'), function() {
 	// Home
 	// --------------------------------------------------------------------------	
 	Route::get('/', function() {
-
-
-		
-		// create default user asset
-		$asset = Asset::where('filename', '=', 'default.png')->first();
-		if($asset == NULL) {
-			$asset = new Asset;
-  			$asset->filename = 'default.png';
-		  	$asset->path = 'assets/content/users'; 
-		  	$asset->save();
-		}
-
-		$admin = Role::where('name', '=', 'Admin')->first();
-
-		// create default roles
-		if($admin == NULL) {
-			$admin = new Role;
-			$admin->name = 'Admin';
-			$admin->save();
-		}
-
-
-		$adminUser = User::where('username', '=', 'admin')->first();
-
-		if($adminUser != NULL) {
-
-			$this->error("Admin User Already Exist");
-
-			if($this->confirm('Remove this Admin User? [yes|no]', true)) {
-				$adminUser->delete();
-				$adminUser = NULL;
-	        }
-
-		}
-		
-		if($adminUser == NULL) {
-			$adminUser = new User;
-
-			$adminUser->username = 'admin';
-			$adminUser->email = 'admin@admin.com';
-			$adminUser->password = 'admin';
-			$adminUser->password_confirmation = 'admin';
-		    $adminUser->confirmation_code = md5(uniqid(mt_rand(), true));
-
-		    if($adminUser->save()) {
-				$adminUser->attachRole( $admin );
-		    	$this->comment("Admin User Created");
-		    }
-		}
-
-		
-
-		
-		return '';
 		return View::make('site.index');
 	});
 
